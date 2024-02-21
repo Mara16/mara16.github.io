b77aa6cd9a942b9ca0e154bdf53717a3e78648e1
Efren Ulloa <eulloa345@gmail.com>
2024-02-21 12:40:41 -0600

    feat: redirect to maraulloa.com

--- js/scroll_to_top.js
+++
@@ -1,5 +0,0 @@
-const scrollToTopBtn = document.getElementById('scrollToTop')
-
-const scroll = () => window.scrollTo({ top: 0, behavior: 'smooth' })
-
-scrollToTopBtn.addEventListener('click', scroll)
